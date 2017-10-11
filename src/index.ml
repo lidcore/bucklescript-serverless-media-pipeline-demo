fa7798214c33f38bbfe1f9a1b9b025d95e062167
Romain Beauxis <toots@rastageeks.org>
2017-10-11 09:19:05 -0500

    s/handler/lambda_function/g

--- src/index.ml
+++ src/index.ml
@@ -1,4 +1,4 @@
-type ('a, 'b) handler = 'a -> unit -> 'b Callback.callback -> unit
+type ('a, 'b) lambda_function = 'a -> unit -> 'b Callback.callback -> unit
 
 let encode_file    = Encode_file.handler
 let queue_encoding = Queue_encoding.handler
--- src/index.mli
+++ src/index.mli
@@ -1,4 +1,4 @@
-type ('a, 'b) handler = 'a -> unit -> 'b Callback.callback -> unit
+type ('a, 'b) lambda_function = 'a -> unit -> 'b Callback.callback -> unit
 
-val encode_file    : (Encode_file.params,unit) handler
-val queue_encoding : (Queue_encoding.params, Api_handler.response) handler
+val encode_file    : (Encode_file.params,unit) lambda_function
+val queue_encoding : (Queue_encoding.params, Api_handler.response) lambda_function
